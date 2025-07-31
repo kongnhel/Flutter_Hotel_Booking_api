1d58daa04d3b61b8aa232caffd3549035d6a2f33
kongnhel <kabkong94@gmail.com>
2025-07-31 13:06:56 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,16 +2,22 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const userRoutes = require("./routes/user.routes.js"); // ✅ ផ្លូវត្រឹមត្រូវ
-const roomRoutes = require("./routes/room.routes.js"); // ✅ ផ្លូវត្រឹមត្រូវ
+const userRoutes = require("./routes/user.routes.js");
+const roomRoutes = require("./routes/room.routes.js");
+const roomTypeRoute = require("./routes/roomType.routes.js");
+const orderRoutes = require("./routes/order.routes.js");
 
 // ✅ Middleware
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api/users", userRoutes);
 app.use("/api/rooms", roomRoutes);
+app.use("/api/room_types", roomTypeRoute);
+app.use("/api/orders", orderRoutes);
 
 // ✅ Start server
 const PORT = 3000;
